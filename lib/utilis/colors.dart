415a93f93b9bc9fcf5ec75cb87048a64a4980c3a
Sajini Weerasinghe <nssweerasinghe@std.appsc.sab.ac.lk>
2024-08-06 17:26:33 +0530

    created color collections

---
+++ lib/utilis/colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+final Color mainTextColor = const Color(0xff3B3636).withOpacity(0.75);
+const Color mainColor = Color(0xff9E00FF);
+const Color mainBlack = Colors.black;
+const Color mainWhite = Colors.white;
+
+const Color firstCategotyColor = Color(0xff8E8FFA);
+const Color secondCategotyColor = Color(0xffC2D9FF);
+const Color thirdCategotyColor = Color(0xffFFE500);
+
+const mainNaturalWondersColor = Color(0xff2EB200);
+final Color subNaturalWondersColor = const Color(0xff2EB200).withOpacity(0.53);
+
+const mainNightLifeColor = Color(0xffFF9900);
+final Color subNightLifeColor = const Color(0xffFF9900).withOpacity(0.52);
+
+const mainLandmarksColor = Color(0xff0029FF);
+final Color subLandmarksColor = const Color(0xff0029FF).withOpacity(0.66);
+final Color landmarkCardBg = const Color(0xffCACACA).withOpacity(0.31);
+
+const mainCulturalColor = Color(0xffC00C00);
+
+const mainGreenColor = Color(0xff06FFA5);
+const mainRedColor = Color(0xffFF1E0F);
